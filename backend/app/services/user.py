0e2fb58aeae52d61fef3cf2a121412d363bfd085
Egor PC <yp27062004@mail.ru>
2026-03-06 20:29:13 +0300

    feat: -Add user_register, user_authentication & user_profile_update methods for UserService

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -12,6 +12,7 @@ class UserService:
         self.session = session
         self.user_repo = user_repo
 
+    # Регистрация пользователя в системе
     async def user_register(self, user_data: UserCreate) -> UserOut:
 
         # Проверка уникальности 
@@ -38,6 +39,7 @@ class UserService:
         # Возвращаем ответ, "прогоняя" его через Pydantic-схему
         return UserOut.model_validate(user)
     
+    # Логин пользователя 
     async def user_authentication(self, phone: str, password: str) -> Optional[UserOut]:
         user = await self.user_repo.get_by_phone(phone)
 
@@ -45,7 +47,35 @@ class UserService:
             return None
         if not verify_password(password, user.password_hash):
             return None
-          
+        
+        return UserOut.model_validate(user)
+        
+    # Обновление профиля пользователя
+    async def user_profile_update(self, user_id : int, user_data : UserUpdate) -> Optional[UserOut]:
+        # Берет в словарь только те поля, которые были переданы
+        updates : dict = user_data.model_dump(exclude_unset=True)
+
+        updated_user = await self.user_repo.update_user(user_id, updates)
+        if not updated_user:
+            return None
+
+        await self.session.commit()
+
+        return UserOut.model_validate(updated_user)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
         
             
 
